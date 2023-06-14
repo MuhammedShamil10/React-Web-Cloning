3c46f9f4122ed1ebb33b14ed61f4cd8b338d9007
shamil <muhammedshamilml10@gmail.com>
2023-06-14 20:09:13 +0530

    feat:updating css

---
+++ azure-clone/src/Component/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+import hamBurger from "/assets/img/hamBurger.svg";
+
+function Header() {
+  return (
+    <>
+      <div>
+        <header>
+          <div className="header-container">
+            <div className=""></div>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default Header;
